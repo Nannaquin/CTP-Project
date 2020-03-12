1db566743353652bb8963af3a286f38a423d0975
Nannaquin <stvnmsantana@gmail.com>
2020-03-12 16:30:00 -0400

    Changed Signup Input Validation Scheme

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,29 +1,39 @@
 const router = require('express').Router();
 const { Users } = require('../models');
+const validateSignupInput = require("../validation/signup");
 const passport = require('../middlewares/authentication');
+
 
 
 router.post('/signup', (req, res) => {
 
-  if(req.body.password == req.body.password2) {
-    console.log("Pass match")
+  try {
+  console.log("Pre validation")
+  const { isValid, errors } = validateSignupInput(req.body)
+
+  if(!isValid) {
+    console.log("Errors present in input")
+    throw(errors)
+  }
+//  if(req.body.password == req.body.password2) {
+    console.log("Passed validations")
     Users.create({
       username: req.body.username,
       email: req.body.email,
       password: req.body.password,
       phonenumber: req.body.phonenumber,
-      auth_token: req.body.email
+      auth_token: req.body.email // do something about this
     })
       .then((user) => {
         console.log("Signup Create Then")
         req.login(user, () => res.status(201).json(user));
       })
       .catch((err) => {
-      
-        const message = err["errors"][0]["message"]
-        console.log(message)
+        console.log("In the catch")
+        //const message = err["errors"][0]["message"]
+        console.log(err)
 
-        let errmsg = "Signup Error" 
+     /*   let errmsg = "Signup Error" 
         if(message == "email must be unique") {
           console.log("Email in use")
           errmsg = "Email already in use"
@@ -33,13 +43,15 @@ router.post('/signup', (req, res) => {
           errmsg = "Phone number already in use"
         } else if(message == "Validation len on password failed") {
           errmsg = "Password too short."
-        }
+        } */
         
-        console.log("Post error check")
-        res.status(400).json({ msg: errmsg  });
+        res.status(400).json({ msg: err  });
       });
-  }
-
+//  }
+    }
+    catch(err) {
+      res.status(400).json({msg: err});
+    }
 });
 
 
